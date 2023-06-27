cb179df08caf0d014e870efcfcae4cf182ea5cc0
Valerie Valentin <valerie.valentine93@gmail.com>
2023-06-27 14:42:50 -0700

    Board Component WIP

--- src/App.js
+++ src/App.js
@@ -2,18 +2,41 @@ import './App.css';
 import React from 'react';
 import { useState } from 'react';
 
+const DATA = 
+  [
+    {
+        "board_id": 1,
+        "title": "Parrots",
+        "owner": "Whitney",
+    },
+    {
+      "board_id": 2,
+      "title": "Cats",
+      "owner": "Valerie",
+    },
+    {
+      "board_id": 3,
+      "title": "Dogs",
+      "owner": "Adrian",
+    }
+]
+
 function App() {
 
-  const [boardsData, setBoardsData] = useState([])
-  const [selectedBoard, setSelectedBoard] = useState(null)
-  const [isBoardFormVisible, setIsBoardFormVisible] = useState(false)
+  const [boardsData, setBoardsData] = useState(DATA);
+  const [selectedBoard, setSelectedBoard] = useState({boardId: null, title: "none selected", owner: "none selected"});
+  const [isBoardFormVisible, setIsBoardFormVisible] = useState(false);
 
+  
 
   return (
     <div className="App">
       <header>
-
       </header>
+      <main>
+        <body>
+        </body>
+      </main>
     </div>
   );
 }
--- src/components/BoardPicker.js
+++ src/components/BoardPicker.js
@@ -1,3 +1,22 @@
 import React from "react";
+// import './BoardPicker.css';
+import Board from './Board';
+// import PropTypes from 'prop-types';
 
 // When we display JSX each element will need an event handler to display the board.
+const BoardPicker = ({boardsData, selectedBoard}) => {
+    const boardComponents = boardsData.map((board) => {
+        return (
+            <li>
+                {board.title}
+            </li>
+        );
+    });
+    return (
+        <section className = 'board-list'>
+            {boardComponents}
+        </section>
+    );
+};
+
+export default BoardPicker;
