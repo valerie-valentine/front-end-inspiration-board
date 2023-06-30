9a53a07bd4f1d32c211bd192c1c1400f1989c4dd
Whitney <whitneyshake@gmail.com>
2023-06-29 22:06:54 -0700

    Like button started

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,12 @@
 import React from "react";
 
 const Card = ({ id, likeCount, message }) => {
+
   return (
     <section>
       <p>{message}</p>
       <p>{likeCount}</p>
+      <button className="like-button" onClick={onUpdateLikes}>❤️</button>
     </section>
   );
 };
